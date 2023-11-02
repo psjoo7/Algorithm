5462ba845c94a7c9ac4feb712337ecceccaad769
ParkSungJung <david@playmed.fun>
2023-11-02 18:46:47 +0900

    DP

---
+++ 이코테/DP/효율적인_화폐_구성.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+coin = []
+for i in range(n):
+    coin.append(int(input()))
+
+d = [10001] * (m+1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(coin[i], m+1):
+        if d[j-coin[i]] != 10001:
+            
