d4b4f23a1dc4df90b3a877c6d9c9396dca1e1ac1
psjoo7 <jmin9480@naver.com>
2024-02-12 23:48:52 +0900

    기초 풀이 중16

---
+++ 단계별백준/스택큐덱/스택2.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+def stack(order, answer):
+    if order[0] == 1:
+        answer.append(order[1])
+    elif order[0] == 2:
+        if len(answer) != 0:
+            print(answer.pop())
+        else:
+            print(-1)
+    elif order[0] == 3:
+        print(len(answer))
+    elif order[0] == 4:
+        if len(answer) == 0:
+            print(1)
+        else:
+            print(0)
+    else:
+        if len(answer) == 0:
+            print(-1)
+        else:
+            print(answer[-1])
+    return answer
+
+answer = list()
+
+for _ in range(n):
+    order = list(map(int, input().split()))
+    answer = stack(order,answer)
