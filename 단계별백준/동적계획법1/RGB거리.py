1157fbf00b0b1a6707a839cfb498f52ceb85cfae
psjoo7 <jmin9480@naver.com>
2024-03-04 21:11:24 +0900

    DP 문제풀이 5

--- 단계별백준/동적계획법1/RGB거리.py
+++ 단계별백준/동적계획법1/RGB거리.py
@@ -8,3 +8,9 @@ for i in range(n):
     r, g, b = map(int, input().split())
     houses.append([r,g,b])
 
+for i in range(1, n):
+    houses[i][0] = min(houses[i-1][1], houses[i-1][2]) + houses[i][0]
+    houses[i][1] = min(houses[i-1][0], houses[i-1][2]) + houses[i][1]
+    houses[i][2] = min(houses[i-1][0], houses[i-1][1]) + houses[i][2]
+
+print(min(houses[n-1]))
